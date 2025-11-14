2226ad4f020e87f36b1f4023068aa5ab7b12af31
mmaarouf <maarouf.9@gmail.com>
2025-11-14 22:16:05 +1100

    add root install script

--- install.sh
+++ install.sh
@@ -1,1 +1,16 @@
 #!/usr/bin/env bash
+set -eu
+
+
+main() {
+    local -r tmp_dir="$(mktemp -d)"
+    pushd "$tmp_dir"
+
+    curl -L https://github.com/mmaarouf/nvim/releases/download/latest/nvim.tar.gz -o nvim.tar.gz
+    tar -xzf nvim.tar.gz
+    bash installer/install.sh
+
+    popd
+}
+
+main
