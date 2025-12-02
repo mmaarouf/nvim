5bfa45dfc7660dd8598520613ef77b8b6d53fdae
mmaarouf <maarouf.9@gmail.com>
2025-12-02 22:21:13 +1100

    update lspconfig to use new setup + tsgo instead of ts_ls

--- nvim-config/lua/plugin/lspconfig.lua
+++ nvim-config/lua/plugin/lspconfig.lua
@@ -8,15 +8,13 @@ local function typescript_setup()
     -- locating the ts-ls bin through a env var
     -- enables ability to point to a script that invokes
     -- the binary within a containerised environment
-    local ts_ls_path = os.getenv('TS_LS_PATH')
+    local tsgo_path = os.getenv('TSGO_PATH')
 
-    if ts_ls_path then
-        require 'lspconfig'.ts_ls.setup {
-            before_init = function(params)
-                params.processId = vim.NIL -- without this the TS Server quits for some reason
-            end,
-            cmd = { ts_ls_path, '--stdio' }
-        }
+    if tsgo_path then
+        vim.lsp.config('tsgo', {
+            cmd = { tsgo_path, '--lsp', '--stdio' }
+        })
+        vim.lsp.enable('tsgo')
     end
 end
 
@@ -24,4 +22,4 @@ function plugin.init()
     typescript_setup()
 end
 
-return plugin;
+return plugin
